3bcd34997e65d30dd3d7d35e3eeda27d4823565f
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2017-10-15 14:29:43 +0900

    use uint_32 uint_64

--- randomGen/xorshift/xorshift.c
+++ randomGen/xorshift/xorshift.c
@@ -1,29 +1,31 @@
+//https://ja.wikipedia.org/wiki/Xorshift
 #include <stdio.h>
+#include <stdint.h>
 
-unsigned int xor32(void)
+uint32_t xor32(void)
 {
-    static unsigned int y = 2463534242;
+    static uint32_t y = 2463534242;
     y = y ^ (y << 13);
     y = y ^ (y >> 17);
     y = y ^ (y << 5);
     return y;
 }
 
-unsigned int xor64(void)
+uint32_t xor64(void)
 {
-    static long x = 88172645463325252ULL;
+    static uint64_t x = 88172645463325252ULL;
     x = x ^ (x << 13);
     x = x ^ (x >> 7);
     x = x ^ (x << 17);
     return x;
 }
 
-unsigned int xor96(void)
+uint32_t xor96(void)
 {
-    static unsigned int x = 123456789;
-    static unsigned int y = 362436069;
-    static unsigned int z = 521288629;
-    unsigned int t;
+    static uint32_t x = 123456789;
+    static uint32_t y = 362436069;
+    static uint32_t z = 521288629;
+    uint32_t t;
     t = (x ^ (x << 3)) ^ (y ^ (y >> 19)) ^ (z ^ (z << 6));
     x = y;
     y = z;
@@ -31,13 +33,13 @@ unsigned int xor96(void)
     return z;
 }
 
-unsigned int xor128(void)
+uint32_t xor128(void)
 {
-    static unsigned int x = 123456789;
-    static unsigned int y = 362436069;
-    static unsigned int z = 521288629;
-    static unsigned int w = 88675123;
-    unsigned int t;
+    static uint32_t x = 123456789;
+    static uint32_t y = 362436069;
+    static uint32_t z = 521288629;
+    static uint32_t w = 88675123;
+    uint32_t t;
     t = x ^ (x << 11);
     x = y;
     y = z;
@@ -48,9 +50,9 @@ unsigned int xor128(void)
 
 int main(void)
 {
-    unsigned int count = 100;
-    for (unsigned int i = 0; i < count; ++i)
+    int count = 100;
+    for (int i = 0; i < count; ++i)
     {
-        printf("%u\n", xor64());
+        printf("%u\n", xor32());
     }
 }
